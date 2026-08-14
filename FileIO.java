f9b7facbd9f4e27829394b61342aac2752eec7b3
Aditya Mudgal <mudgal.aaditya@gmail.com>
2026-08-14 11:35:07 +0530

    changes

--- FileIO.java
+++ FileIO.java
@@ -4,23 +4,58 @@ public class FileIO {
     public static void main(String[] args) throws IOException{
         int i;
         FileInputStream fin;
+        FileOutputStream fout;
+
+        // try{
+        //     fin = new FileInputStream("the_echoing_cave.txt");
+        // }catch(FileNotFoundException e){
+        //     System.out.println("File Not Found");
+        //     return;
+        // }catch(ArrayIndexOutOfBoundsException e){
+        //     System.out.println("Usage: Show File ");
+        //     return;
+        // }
+
+        // do{
+        //     i = fin.read();
+        //     if(i != -1) System.out.print((char) i);
+        // }while(i  != -1);
 
         try{
-            fin = new FileInputStream("the_echoing_cave.txt");
-        }catch(FileNotFoundException e){
-            System.out.println("File Not Found");
-            return;
+            try{
+                fin = new FileInputStream("the_echoing_cave.txt");
+
+            }catch(FileNotFoundException e){
+                System.out.println("Input File Not Found");
+                return;
+            }
+            try{
+                fout = new FileOutputStream("example.txt");
+            }catch(FileNotFoundException e){
+                System.out.println("File not found");
+                return;
+            }
         }catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Usage: Show File ");
+            System.out.println("Usage : CopyFile form to");
             return;
         }
 
-        do{
-            i = fin.read();
-            if(i != -1) System.out.print((char) i);
-        }while(i  != -1);
+        //copy file
+        try{
+            do{
+                i = fin.read();
+                if(i != -1){
+                    fout.write(i);
+                }
+            }while(i != -1);
+        }catch(IOException e){
+                System.out.print("File Error");
+            }
 
         fin.close();
+        fout.close();
+        }
         
-    }
+        
 }
+
